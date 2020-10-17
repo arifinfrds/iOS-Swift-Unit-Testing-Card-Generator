21d8a0259826d9fffcfe418d638f66bee7d94a79
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2020-10-17 11:32:02 +0700

    Initial Commit

---
+++ Card/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Card
+//
+//  Created by Arifin Firdaus on 17/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
